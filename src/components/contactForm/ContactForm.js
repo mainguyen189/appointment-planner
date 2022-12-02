59560e938810eee16b512aa5728d36dd5e20bf8d
mion158 <111487792+mion158@users.noreply.github.com>
2022-12-02 19:35:21 +0000

    added contact form components

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -7,9 +7,39 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+        placeholder="Input contact name"
+      />
+
+      <input
+        type="tel"
+        name="phone"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        required
+        pattern="^(?:0|\+?44)(?:\d\s?){9,10}$"
+        placeholder="Input phone number"
+      />
+
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        placeholder="Input email address"
+      />
+
+      <input type="submit" value="Add contact" />
+    </form>
   );
 };
